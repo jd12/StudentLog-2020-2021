772ebdeb5a685797ceee8eb62ae448f4331c23c1
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2022-09-21 13:49:30 -0700

    feat: Complete student log script

---
+++ scripts/start-entry.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+FILENAME="logs/$(date +'%Y-%m-%d').log.md"
+touch $FILENAME
+echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
+echo "- [ ]" >> $FILENAME
+/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code $FILENAME
